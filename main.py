da5310ad0fffbce0e171e3d71709ef0d1ab95770
Justin D'Souza <justinqdsouza@gmail.com>
2018-12-22 19:01:40 -0500

    fixed error handling:

--- main.py
+++ main.py
@@ -4,19 +4,22 @@ import time
 if __name__ == "__main__":
 	try: 
 		lower_bound = int(input("Enter a lower bound integer: "))
-		upper_bound = input("Enter an upper bound integer: ")
-		num_seconds = input("Enter the length of time (in seconds) that you'd like to be tested for: ")
+		upper_bound = int(input("Enter an upper bound integer: "))
+		num_seconds = int(input("Enter the length of time (in seconds) that you'd like to be tested for: "))
 	except ValueError:
-		print("Enter a number only.")
+		print("Error: enter a number only.")
+		raise
 
 	try:
-		operations = raw_input("Select from the following operations: multiplication, addition, subtraction, division, exponentiation, perfect squares, square roots. Type 'all' if you'd like to be tested on every operation: ")
+		operations = input("Select from the following operations: multiplication, addition, subtraction, division, exponentiation, perfect squares, square roots. Type 'all' if you'd like to be tested on every operation: ")
 	except ValueError:
-		print("Enter a string only.")
+		print("Error: select only one of the following options")
+		raise
 
 	question_obj = QuestionGenerator(num_seconds, operations, lower_bound, upper_bound)
 	start_time = time.time()
+	print(start_time)
 	
-	while (time.time() - start.time) < num_seconds:
+	while time.time() < start_time + num_seconds:
 		question_obj.generate_question()
 	
